6b928db5707d5913a1702ce4ae6725cfe09c8df8
BRUNO Elie <elie.bruno@proton.ch>
2024-09-19 18:26:56 +0100

    Cleaned Dataset with derived values

--- cleaning/clean_dataset.py
+++ cleaning/clean_dataset.py
@@ -5,19 +5,42 @@ def clean_netflix_data(input_file, output_file):
     df = pd.read_csv(input_file)
 
     # Columns to be removed
-    columns_to_remove = ['Tags', 'Runtime', 'View Rating', 'Production House', 
-                         'Netflix Link', 'IMDb Link', 'Summary', 'IMDb Votes', 
-                         'Poster', 'TMDb Trailer', 'Trailer Site', 'Hidden Gem Score',]
+    columns_to_remove = [
+        'Tags', 'Runtime', 'View Rating', 'Production House', 
+        'Netflix Link', 'IMDb Link', 'Summary', 'IMDb Votes', 
+        'Poster', 'TMDb Trailer', 'Trailer Site', 'Hidden Gem Score', 
+        'Awards Received', 'Awards Nominated For', 'Boxoffice'
+    ]
 
     # Remove the specified columns
     df_cleaned = df.drop(columns=columns_to_remove)
 
-    # Save the cleaned DataFrame to a new CSV file
+    # Rescale IMDb Score (scale of 1-10 to 100)
+    df_cleaned['IMDb Score'] = df_cleaned['IMDb Score'] * 10
+
+    # Create a new column 'Average Score' by averaging the three scores
+    df_cleaned['Average Score'] = df_cleaned[
+        ['IMDb Score', 'Rotten Tomatoes Score', 'Metacritic Score']
+    ].mean(axis=1, skipna=True)
+
+    # Convert 'Release Date' to 'YYYY-MM-DD' format
+    df_cleaned['Release Date'] = pd.to_datetime(
+        df_cleaned['Release Date'], format='%d %b %Y'
+    ).dt.strftime('%Y-%m-%d')
+
+    # Calculate 'Days Until Netflix Release'
+    df_cleaned['Days Until Netflix Release'] = (
+        pd.to_datetime(df_cleaned["Netflix Release Date"]) - 
+        pd.to_datetime(df_cleaned["Release Date"])
+    ).dt.days
+
+    # Save the cleaned DataFrame with the new 'Average Score' and formatted 'Release Date' to a new CSV file
     df_cleaned.to_csv(output_file, index=False)
-    print(f"Cleaned data saved to {output_file}")
+    print(f"Cleaned data with average score and formatted release date saved to {output_file}")
 
-# Example usage of the pipeline function
+# Example usage
 input_file = './dataset/raw-netflix-rotten-tomatoes-metacritic-imdb.csv'
-output_file = './dataset/cleaned_netflix_data.csv'
+output_file = './dataset/cleaned_netflix_data_with_avg_and_date.csv'
 
+# Run this function locally
 clean_netflix_data(input_file, output_file)
--- cleaning/list_header.py
+++ cleaning/list_header.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the CSV file
-file_path = './dataset/raw-netflix-rotten-tomatoes-metacritic-imdb.csv'
+file_path = './dataset/cleaned_netflix_data_with_avg_and_date.csv'
 df = pd.read_csv(file_path)
 
 # List all the header types (column names and their data types)
