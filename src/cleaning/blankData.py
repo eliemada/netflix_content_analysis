6849653e74e1fcddc7ef92442e196b0cdbfd9d1f
BRUNO Elie <elie.bruno@proton.ch>
2024-09-26 14:31:14 +0100

    Fix

---
+++ src/cleaning/blankData.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load the CSV file
+file_path = './dataset/cleaned_netflix_data.csv'
+df = pd.read_csv(file_path)
+# Check for missing or blank values
+missing_data = df.isnull().sum()
+
+# Display the columns with missing values
+print("Missing or Blank values in each column:")
+print(missing_data[missing_data > 0])
