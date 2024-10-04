265d574bb04a542fd11a5fcb420354cb2d2688a6
BRUNO Elie <elie.bruno@proton.ch>
2024-10-04 15:02:33 +0100

    Fixed dataset TV shows

--- src/cleaning/dropBlanks.py
+++ src/cleaning/dropBlanks.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 # Load the dataset
-file_path = './dataset/cleaned_netflix_data_with_avg_and_date.csv'  # Assuming the CSV file is uploaded to the environment
+file_path = './src/dataset/cleaned_netflix_data_with_avg_and_date.csv'  # Assuming the CSV file is uploaded to the environment
 df = pd.read_csv(file_path)
 
 
 # Remove rows where any of the specified columns have missing values or blanks
-columns_to_check = ['Title', 'Genre', 'Languages', 'Series or Movie', 'Director', 'Writer','Actors', 'Country Availability', 'Release Date', 'Netflix Release Date', 'Days Until Netflix Release', 'Average Score', 'Image']
+columns_to_check = ['Title', 'Genre', 'Languages', 'Series or Movie','Actors', 'Country Availability', 'Release Date', 'Netflix Release Date', 'Days Until Netflix Release', 'Average Score', 'Image']
 df_cleaned = df.dropna(subset=columns_to_check)
-cols = ['Writer','IMDb Score', 'Rotten Tomatoes Score', 'Metacritic Score']
+cols = ['IMDb Score', 'Rotten Tomatoes Score', 'Metacritic Score']
 df_cleaned = df_cleaned.drop(columns=cols)
 
 
 # Saving the cleaned DataFrame back to CSV
-cleaned_file_path = './dataset/cleaned_netflix_data.csv'
+cleaned_file_path = './src/dataset/cleaned_netflix_data.csv'
 df_cleaned.to_csv(cleaned_file_path, index=False)
 
 cleaned_file_path
